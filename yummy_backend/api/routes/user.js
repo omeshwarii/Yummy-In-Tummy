18a75fb1a5c6a360f4d12bb148997929af253247
Omeshwari Jakkula <jakko01@pfw.edu>
2024-03-23 19:22:58 -0400

    backend new changes

--- yummy_backend/api/routes/user.js
+++ yummy_backend/api/routes/user.js
@@ -3,9 +3,5 @@ const router = express.Router();
 
 const UserController = require('../controllers/user');
 
-router.put("/signup", UserController.user_signup);
-
-router.post("/login", UserController.user_login);
-
 router.get("/menu", UserController.get_menu);
 module.exports = router;
